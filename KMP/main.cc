877272a3c1208d0ab38505fbecdb2a80693cfc1d
sosohu <ustc.sosohu@gmail.com>
2014-11-07 10:53:01 +0800

    still TLE

--- KMP/main.cc
+++ KMP/main.cc
@@ -12,45 +12,58 @@ public:
     int KMP(string &target, string &mode) {
 		int lm = mode.length();
 		int lt = target.length();
+		if(lm*lt == 0)	return 0;
 		vector<int> part(lm, 0);
 
 		// init for the part array.
+		// this will be TLE.
 		for(int i = 1; i < lm; i++){
 			int pre = part[i-1];
 			if(mode[i] == mode[pre]){
-				part[i] = pre + 1;
-			}else{
-				int j, k;
-				for(j = pre; j > 0; j--){
-					for(k = 0; k < j; k++){
-						if(mode[k] != mode[i-j+1+k])
-							break;
-					}
-					if(k == j){
-						part[i] = j;
+				part[i] = pre+1;
+				continue;
+			}
+			int pos = i - pre;
+			while(pos <= i){
+				int k, p;
+				int len = i - pos + 1;
+				for(k = 0, p = pos; k < len; k++, p++){
+					if(mode[k] != mode[p]){
+						pos = pos + k + 1 - part[k-1];
 						break;
 					}
+				}
+				if(k == len){
+					part[i] = k;
+					break;
 				}
 			}
 		}
 
-		vector<int> next(lm, 0);
-		for(int i = 0; i < lm; i++){
-			next[i] = i + 1 - part[i];
+		vector<int> next(lm, -1);
+		for(int i = 1; i < lm; i++){
+			#ifdef DEBUG
+			cout<<part[i]<<endl;
+			#endif
+			next[i] = part[i-1];
 		}
 
+		// search the mode str by the next array.
 		int count = 0;
-		for(int pos = 0; pos < lt;){
+		for(int pos = 0; pos < lt - lm + 1; pos++){
 			int i, j;
 			for(j = 0, i = pos; j < lm; i++, j++){
 				if(mode[j] != target[i]){
-					pos = pos + next[j];
-					break;
+					if(next[j] != -1){
+						j = next[j];
+						continue;
+					}else{
+						break;
+					}
 				}	
 			}
 			if(j == lm){
 				count++;
-				pos = pos + next[j-1];
 			}
 		}
 		return count;
