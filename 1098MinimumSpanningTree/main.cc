0564791dc977ff5bd42209bd0c7f33aad057764a
sosohu <ustc.sosohu@gmail.com>
2015-01-16 16:40:16 +0800

    TLE

--- 1098MinimumSpanningTree/main.cc
+++ 1098MinimumSpanningTree/main.cc
@@ -1,38 +1,53 @@
 #include <iostream>
 #include <climits>
 #include <cstdio>
-#include <map>
-#include <set>
 #include <vector>
+#include <sys/time.h>
+#include <algorithm>
 
 using namespace std;
 
-int root(int x, vector<int> &father){
-	return x == father[x]? x : root(father[x], father);
+int father[100001];
+
+struct Node{
+	int w;
+	int s;
+	int e;
+};
+
+Node table[1000001];
+
+inline int root(int x){
+	return x == father[x]? x : root(father[x]);
 }
 
-bool join(int x, int y, vector<int> &father){
-	if(root(x, father) == root(y, father))	return false;
+inline bool join(int x, int y){
+	if(root(x) == root(y))	return false;
 	else{
-		father[root(x, father)] = root(y, father);
+		father[root(x)] = root(y);
 		return true;
 	}
 }
 
-int getMinSpanningTree(multimap<int, pair<int, int> > &data, vector<int> &father, int vecnum){
+int getMinSpanningTree(int vecnum, int arcnum){
 	int count = 0;
 	int s, e;
 	int sum = 0;
-	for(multimap<int, pair<int, int> >::iterator iter = data.begin(); 
-		iter != data.end() && count < vecnum; iter++){
-		s = iter->second.first;
-		e = iter->second.second;
-		if(join(s, e, father))
-			sum = sum + iter->first;
+	for(int i = 0; i < arcnum && count < vecnum; i++){
+		s = table[i].s;
+		e = table[i].e;
+		if(join(s, e))
+			sum = sum + table[i].w;
 	}
 	return sum;
 }
 
+class Cmp{
+public:
+	bool operator()(Node a, Node b){
+		return a.w < b.w;
+	}
+};
 
 int main(int argc, char** argv)
 {
@@ -42,19 +57,31 @@ int main(int argc, char** argv)
 	int vecnum, arcnum;
 	cin>>vecnum>>arcnum;
 	if(vecnum <= 0 || arcnum <= 0 || vecnum > arcnum + 1)	return 0;
-	multimap<int, pair<int, int> > table;
-	vector<int> father(vecnum, -1);
 	for(int i = 0; i < vecnum; i++){
 		father[i] = i;
 	}
 	int s, e, w;
-	for(int i = 0; i < arcnum; i++){
-		cin>>s>>e>>w;
+	int count = 0;
+	for(int i = 0; i < arcnum; i++, count++){
+		//cin>>s>>e>>w;
+		scanf("%d%d%d", &s, &e, &w);
 		s--;
 		e--;
-		table.insert(make_pair(w, make_pair(s, e)));
+		table[count].w = w; 
+		table[count].s = s; 
+		table[count].e = e; 
 	}
-	int ret = getMinSpanningTree(table, father, vecnum);
+	sort(table, table + count, Cmp());
+	#ifdef DEBUG
+	struct timeval start, end;
+	gettimeofday(&start, NULL );
+	#endif
+	int ret = getMinSpanningTree(vecnum, arcnum);
 	cout<<ret<<endl;
+	#ifdef DEBUG
+	gettimeofday( &end, NULL );
+	double timeuse = 1000000 * ( end.tv_sec - start.tv_sec ) + end.tv_usec - start.tv_usec; 
+	cout<<timeuse/1000000<<endl;
+	#endif
 	return 0;
 }
